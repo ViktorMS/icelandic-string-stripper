a7d4570c88f2de2e80da486392b14f07ee9cc8cb
Viktor.ms <vjh2@hi.is>
2019-10-19 20:39:18 +0000

    Strip everything but icelandic alphabet and spaces

--- strip.py
+++ strip.py
@@ -1,12 +1,15 @@
-alphabet = ["a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m","n","o","ó","p","r","s","t","u","ú","v","x","y","ý","þ","æ","ö"]
+def strip(string, replaceWith = " "):
 
-string = "Upphrópun!! Kennitala 255599-3399..."
-alphabet_string = ""
+    alphabet = [" ","a","á","b","d","ð","e","é","f","g","h","i","í","j","k","l","m","n","o","ó","p","r","s","t","u","ú","v","x","y","ý","þ","æ","ö"]
 
-for character in string:
-    if(character.lower() in alphabet):
-        alphabet_string += character
-    else:
-        alphabet_string += " "
+    alphabet_string = ""
+    
+    for character in string:
+        if(character.lower() in alphabet):
+            alphabet_string += character
+        else:
+            alphabet_string += replaceWith
+    return alphabet_string
         
-print(alphabet_string) # Upphrópun   Kennitala
+print(strip("Kennitala 255599-3399..!")) # Kennitala
+print(strip("Kennitala 255599-3399..!", replaceWith = "*")) # Kennitala ************** 
